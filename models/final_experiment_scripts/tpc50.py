97122934e770ec6e09401fc8bc85ec45d09893ba
EmmaRocheteau <ecr38@cam.ac.uk>
2020-05-14 23:49:18 +0100

    fix

--- models/final_experiment_scripts/tpc50.py
+++ models/final_experiment_scripts/tpc50.py
@@ -14,10 +14,10 @@ if __name__=='__main__':
 
     log_folder_path = create_folder('models/experiments/final', c.exp_name)
 
-    tpc = TPC(config=c,
-              n_epochs=c.n_epochs,
-              name=c.exp_name,
-              base_dir=log_folder_path,
-              explogger_kwargs={'folder_format': '%Y-%m-%d_%H%M%S{run_number}'})
     for i in range(10):
+        tpc = TPC(config=c,
+                  n_epochs=c.n_epochs,
+                  name=c.exp_name,
+                  base_dir=log_folder_path,
+                  explogger_kwargs={'folder_format': '%Y-%m-%d_%H%M%S{run_number}'})
         tpc.run()
