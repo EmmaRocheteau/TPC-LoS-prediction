35261b0878b631176ffbc3d3baba4dd31d993a72
EmmaRocheteau <ecr38@cam.ac.uk>
2020-08-04 18:34:57 +0100

    little change

--- interpretability/run_integrated_gradients.py
+++ interpretability/run_integrated_gradients.py
@@ -5,62 +5,60 @@ from captum.attr import IntegratedGradients
 from pathlib import Path
 
 time_point = 23  # this means 24 hours because of python's indexing
-batch_size = 2
+batch_size = 8
 
-if __name__=='__main__':
+p = Path('interpretability').glob('**/*.csv')
+csv_files = [x for x in p if x.is_file()]
+for c in csv_files:
+    c.unlink()
 
-    p = Path('interpretability').glob('**/*.csv')
-    csv_files = [x for x in p if x.is_file()]
-    for c in csv_files:
-        c.unlink()
+base_dir = 'models/experiments/final/TPC'
+exp_dir = 'models/experiments/final/TPC/2020-06-27_2048171'
 
-    base_dir = 'models/experiments/final/TPC'
-    exp_dir = 'models/experiments/final/TPC/2020-06-27_2048171'
+tpc = TPC(base_dir=base_dir,
+          resume=exp_dir,
+          resume_save_types=('model',
+                             'simple',
+                             'th_vars',
+                             'results'))
 
-    tpc = TPC(base_dir=base_dir,
-              resume=exp_dir,
-              resume_save_types=('model',
-                                 'simple',
-                                 'th_vars',
-                                 'results'))
+c = initialise_tpc_arguments()
+c['mode'] = 'test'
+c['exp_name'] = 'TPC'
+c['model_type'] = 'tpc'
+tpc.config = c
 
-    c = initialise_tpc_arguments()
-    c['mode'] = 'test'
-    c['exp_name'] = 'TPC'
-    c['model_type'] = 'tpc'
-    tpc.config = c
+tpc.setup()
+tpc._setup_internal()
+tpc.prepare()
+tpc.model.eval()
 
-    tpc.setup()
-    tpc._setup_internal()
-    tpc.prepare()
-    tpc.model.eval()
+ig = IntegratedGradients(tpc.model)
 
-    ig = IntegratedGradients(tpc.model)
+test_batches = tpc.test_datareader.batch_gen(batch_size=batch_size)
 
-    test_batches = tpc.test_datareader.batch_gen(batch_size=batch_size)
+N = 0
+target = time_point - 5  # The minus 5 is to compensate for the first five hours where predictions are not given.
 
-    N = 0
-    target = time_point - 5  # The minus 5 is to compensate for the first five hours where predictions are not given.
+for i, (padded, mask, diagnoses, flat, labels, seq_lengths) in enumerate(test_batches):
 
-    for i, (padded, mask, diagnoses, flat, labels, seq_lengths) in enumerate(test_batches):
+    # if at least one sequence is longer than 24hrs
+    if max(seq_lengths.cpu().numpy()) - target > 0:
 
-        # if at least one sequence is longer than 24hrs
-        if max(seq_lengths.cpu().numpy()) - target > 0:
+        attr = ig.attribute((padded, diagnoses, flat), target=target)
 
-            attr = ig.attribute((padded, diagnoses, flat), target=target)
+        # day_data is an array containing the indices of patients who stayed at least 24 hours (or up to `timepoint')
+        day_data = mask[:, target].numpy().flatten().nonzero()[0]
+        # we only include patients who are indexed in `day_data' from now on, we can define the number of these patients as B
+        # ts is an array containing the sum of the absolute values for the integrated gradient attributions (the sum is taken across timepoints)
+        ts = np.abs(attr[0].detach()[:, :, :time_point].cpu().numpy()[day_data]).sum(axis=2)  # B x (2F + 2)
+        diag = attr[1].detach().cpu().numpy()[day_data]  # B x D
+        flat = attr[2].detach().cpu().numpy()[day_data]  # B x f
 
-            # day_data is an array containing the indices of patients who stayed at least 24 hours (or up to `timepoint')
-            day_data = mask[:, target].numpy().flatten().nonzero()[0]
-            # we only include patients who are indexed in `day_data' from now on, we can define the number of these patients as B
-            # ts is an array containing the sum of the absolute values for the integrated gradient attributions (the sum is taken across timepoints)
-            ts = np.abs(attr[0].detach()[:, :, :time_point].cpu().numpy()[day_data]).sum(axis=2)  # B x (2F + 2)
-            diag = attr[1].detach().cpu().numpy()[day_data]  # B x D
-            flat = attr[2].detach().cpu().numpy()[day_data]  # B x f
+        for ar, fname in ((ts, 'ts.csv'), (flat, 'flat.csv'), (diag, 'diag.csv')):
+            with open('interpretability/'+fname, 'ba') as f:
+                np.savetxt(f, ar, delimiter=',', fmt='%f')
 
-            for ar, fname in ((ts, 'ts.csv'), (flat, 'flat.csv'), (diag, 'diag.csv')):
-                with open('interpretability/'+fname, 'ba') as f:
-                    np.savetxt(f, ar, delimiter=',', fmt='%f')
-
-            N += len(day_data)
-            if i % 100 == 0:
-                print('Done ' + str(N))
+        N += len(day_data)
+        if i % 100 == 0:
+            print('Done ' + str(N))
